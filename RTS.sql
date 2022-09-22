2106d1ab90e2f306ce9d4f17d1a808158cadc50d
Cyprian11 <barabaszcyprianti@gmail.com>
2022-09-22 11:07:13 +0200

    sql

---
+++ RTS.sql
@@ -0,0 +1,20 @@
+INSERT INTO players (name,email,password) VALUES ("Adam","adam@gmail.com","123");
+INSERT INTO players (name,email,password) VALUES ("Rafal","rafal@gmail.com","456");
+INSERT INTO players (name,email,password) VALUES ("Szymon","szymon@gmail.com","789");
+INSERT INTO players (name,email,password) VALUES ("Cyprian","cyprian@gmail.com","10");
+
+INSERT INTO resources (name,description) VALUES ("drewno","służy do budowania");
+INSERT INTO resources (name,description) VALUES ("glina","służy do budowania");
+INSERT INTO resources (name,description) VALUES ("żelazo","służy do budowania");
+
+
+INSERT INTO troops (name,description,damage,distance_damage,defence) VALUES ("łucznik","strzela","5","20","10");
+INSERT INTO troops (name,description,damage,distance_damage,defence) VALUES ("miecznik","walczy","30","5","15");
+INSERT INTO troops (name,description,damage,distance_damage,defence) VALUES ("kusznik","strzela","10","25","5");
+INSERT INTO troops (name,description,damage,distance_demage,defence) VALUES ("rycerz","walczy","30","5","30");
+
+INSERT INTO troops (name) VALUES ("dół z gliną");
+INSERT INTO troops (name) VALUES ("huta żelaza");
+INSERT INTO troops (name) VALUES ("chata drwala");
+
+ 
